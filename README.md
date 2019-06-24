a15fae62f8453648d7a3d1104f10ff387d9f1c42
Clay McLeod <Clay.McLeod@STJUDE.org>
2019-06-23 20:21:29 -0500

    Seed repository with initial files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# St. Jude Cloud RFCs
+
+This repository contains all of the active RFCs for the St. Jude Cloud project. This repository is currently under development.
---
+++ scripts/generate.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+[[ ! -d src ]] && mkdir src
+echo "[Introduction](introduction.md)\n" > src/SUMMARY.md
+
+for f in $(ls text/* | sort)
+do
+    echo "- [$(basename $f ".md")]($(basename $f))" >> src/SUMMARY.md
+    cp $f src
+done
+
+cp README.md src/introduction.md
+mdbook build
